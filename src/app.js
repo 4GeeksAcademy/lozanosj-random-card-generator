c399316d830a8f1965dec2fcad70c98f1900e6d8
Jhonny Lozano <110267545+lozanosj@users.noreply.github.com>
2024-02-29 02:37:57 +0000

    Logica terminada, falta terminar los estilos :|

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let type = ["♦", "♥", "♠", "♣"];
+
+  function generateRandomCard() {
+    let indexRandNumber = Math.floor(Math.random() * numbers.length);
+    let indexRandType = Math.floor(Math.random() * type.length);
+
+    let randNumber = numbers[indexRandNumber];
+    let randType = type[indexRandType];
+
+    return { number: randNumber, type: randType };
+  }
+
+  let card = generateRandomCard();
+
+  document.querySelector("#number").innerHTML = card.number;
+  document.getElementById("type1").innerHTML = card.type;
+  document.getElementById("type2").innerHTML = card.type;
+
+  let fontColor = card.type === "♥" || card.type === "♦" ? "red" : "black";
+
+  document.getElementById("type1").style.color = fontColor;
+  document.getElementById("type2").style.color = fontColor;
+
+  console.log(card);
 };
